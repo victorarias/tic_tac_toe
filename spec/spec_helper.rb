e859dbbef68c1f259715314338410d17f7040acf
Victor Arias <arias.sp@gmail.com>
2015-08-19 21:14:47 +0100

    Require marks by default and define shortcuts
    It simplify testing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 require "attr_extras"
 
+require "tic_tac_toe/marks"
+
+NoMark = TicTacToe::NoMark
+XMark = TicTacToe::XMark
+OMark = TicTacToe::OMark
+
 RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
