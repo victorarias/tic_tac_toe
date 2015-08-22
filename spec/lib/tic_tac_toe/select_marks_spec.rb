e3e48ecaec3d7f9afccf11c5a51417e3e5d858a8
Victor Arias <arias.sp@gmail.com>
2015-08-22 12:37:41 +0100

    Add specs for extracted SelectMarks

---
+++ spec/lib/tic_tac_toe/select_marks_spec.rb
@@ -0,0 +1,27 @@
+require "tic_tac_toe/select_marks"
+
+describe TicTacToe::SelectMarks, "#call" do
+  it "selects X for player and O for computer when the player selects X" do
+    allow(TicTacToe::ChoosePlayerMark).to receive(:call).with(client: client).and_return(XMark)
+
+    player_mark, computer_mark = TicTacToe::SelectMarks.call(client)
+
+    expect(player_mark).to eq(XMark)
+    expect(computer_mark).to eq(OMark)
+  end
+
+  it "selects O for player and X for computer when the player selects O" do
+    allow(TicTacToe::ChoosePlayerMark).to receive(:call).with(client: client).and_return(OMark)
+
+    player_mark, computer_mark = TicTacToe::SelectMarks.call(client)
+
+    expect(player_mark).to eq(OMark)
+    expect(computer_mark).to eq(XMark)
+  end
+
+  private
+
+  def client
+    @client ||= double
+  end
+end
